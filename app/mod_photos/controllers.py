620c02973bdc372acd0509056d167e57ecd6c9d8
Carlos B <carlos.baezruiz@gmail.com>
2016-04-13 00:42:10 +0200

    First implementation for photo uploader

--- app/mod_photos/controllers.py
+++ app/mod_photos/controllers.py
@@ -1,17 +1,27 @@
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for, jsonify, json
 from werkzeug import check_password_hash, generate_password_hash
 from app import app
 from app.data import db
 
 from app.mod_photos.models import Photo
 
+import uuid
+import os
+
 mod_photos = Blueprint('photos',__name__,url_prefix='/photos')
+
 
 
 @mod_photos('/photos/',methods=['POST'])
 def photos():
 	app.logger.debug("Applying post photo...")
-	
+	if request.method == 'POST':
+		file = request.files['file']
+		extension = os.path.splitext(file.filename)[1]
+		f_name=str(uuid.uuid4()) + extension
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+		return json.dumps({'filename':f_name})
+		
 
 '''
 def check_password(pass1 , pass2):
--- app/mod_photos/tests.py
+++ app/mod_photos/tests.py
@@ -3,6 +3,7 @@ from flask.ext.login import current_user
 
 from app.test_base import BaseTestCase
 from app import app
+
 from .models import Photo
 
 
@@ -10,11 +11,9 @@ class TestModelPhoto(BaseTestCase):
     def test_new_photo(self):
         photo = Photo('carlos')
         self.assertTrue(photo.name,'carlos')
-        print photo.id
 
 class TestPhotosViews(BaseTestCase):
     def test_post_photo(self):
         with self.app.open_resource("test_resources/photo.jpg") as fp:
             with self.client:
-                response = self.client.post(url_for('photos'),data={'upload':fp});
- 
+                response = self.client.post(url_for('/photos/photos'),data={'file':fp});
